7d43121a7d04c0d0a2c7b78668483e865eb0edc5
Aman Pratap Singh <aman.singh@perennialsys.com>
2026-01-05 12:30:48 +0530

    Initial App

---
+++ PereMicroBanking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PereMicroBanking
+//
+//  Created by Aman Singh on 05/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PereMicroBanking/PereMicroBankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PereMicroBankingApp.swift
+//  PereMicroBanking
+//
+//  Created by Aman Singh on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PereMicroBankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
